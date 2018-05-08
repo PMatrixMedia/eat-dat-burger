50b9f7d0dd2502c244df997416b80a4684e742d3
Phase Matrix <pmatrixmedia@gmail.com>
2018-05-08 11:53:00 -0500

    version 1.0.0.2

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -18,9 +18,9 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "ingredents", "devoured"
+    "name", "devoured"
   ], [
-    req.body.ingredents, req.body.devoured
+    req.body.name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
