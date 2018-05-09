493eca8c968533543b964a41c81c818ea6acfdf9
Phase Matrix <pmatrixmedia@gmail.com>
2018-05-08 22:30:49 -0500

    version 2.0.0.2

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -5,13 +5,13 @@ $(function() {
     var newBurger = $(this).data("newBurger");
 
     var newBurgerState = {
-      devoured: newBurger
+      devoured: false
     };
 
     // Send the PUT request.
     $.ajax("/burgers/" + id, {
       type: "PUT",
-      data: newBurgerState
+        data: { devoured: true }
     }).then(
       function() {
         console.log("changed devoured to", newBurger);
@@ -31,14 +31,15 @@ $(function() {
     };
 
     // Send the POST request.
-    $.ajax("/burgers", {
-      type: "POST",
-      data: newBurger
+      $.ajax("/burgers", {
+          type: "POST",
+          data: { name: newBurger }
     }).then(
       function() {
         console.log("created a new burger");
         // Reload the page to get the updated list
-        location.reload();
+          (location.reload());
+          $('#newBurger').val('');
       }
     );
   });
