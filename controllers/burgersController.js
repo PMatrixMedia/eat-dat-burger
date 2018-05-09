4136d3d9f1180577e22b2ab1a38dd7af1a4d90eb
Phase Matrix <pmatrixmedia@gmail.com>
2018-05-08 23:35:10 -0500

    version 2.0.0.1

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+var burgers = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burgers.all(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create([
+  burgers.create([
     'Burger_name', 'devoured'
   ], [
     req.body.Burger_name, req.body.devoured
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
+  burgers.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 1) {
